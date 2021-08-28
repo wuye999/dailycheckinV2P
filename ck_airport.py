3f080fdc20facc2c2d78109051dbcbe2cac1f47e
Leon <62660101+Oreomeow@users.noreply.github.com>
2021-08-29 05:40:16 +0800

    🧪

--- ck_airport.py
+++ ck_airport.py
@@ -82,7 +82,7 @@ class SspanelQd(object):
             email = str(airport_account.get("airport_email"))
             password = str(airport_account.get("airport_password"))
             msg = self.checkin(url,email,password)
-            msg_all += msg + '\n\n'
+            msg_all += url + '\n' + msg + '\n\n'
         return msg_all
 
 def start():
